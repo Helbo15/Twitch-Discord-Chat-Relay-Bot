c4fa62d92e9d3419bfa80f2a44057e6c9086f42b
Helbo15 <helbo15@gmail.com>
2018-11-19 20:10:02 +0100

    Updated DiscordChat
    Added code to ensure that the only channel the bot is listening on is the Specified Channel in the ConfigFile

--- DiscordChat.py
+++ DiscordChat.py
@@ -8,11 +8,12 @@ import SmilieMap
 from SmilieMap import SmilieTranslation
 
 class DiscordChat:
-    def __init__(self,webHookId,WebHookToken,DiscordBotToken):
+    def __init__(self,webHookId,WebHookToken,DiscordBotToken,ChannelName):
         self.messageQueue = Queue()
         self.client = discord.Client()
         self.on_ready = self.client.event(self.on_ready)
         self.on_message  = self.client.event(self.on_message)
+        self.channelName = ChannelName
         self.WEBHOOK_ID = webHookId  
         self.WEBHOOK_TOKEN = WebHookToken  
         self.DISCORD_BOT_TOKEN = DiscordBotToken 
@@ -29,12 +30,13 @@ class DiscordChat:
         print('------')
         
     async def on_message(self,message):
-        if message.author == self.client.user:
-            return
-        if message.author.bot:
-            return
-        message.content = self.translateEmoji(message.content)
-        self.messageQueue.put_nowait(message)
+        if message.channel.name == self.channelName:
+            if message.author == self.client.user:
+                return
+            if message.author.bot:
+                return
+            message.content = self.translateEmoji(message.content)
+            self.messageQueue.put_nowait(message)
 
     def run(self):
         self.client.run(self.DISCORD_BOT_TOKEN)
